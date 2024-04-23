ef1917f87806bfe8e22198d61faa2777259f6d94
Nitin Gumber <aroranitin486@gmail.com>
2024-04-23 12:09:20 +0530

    add: script type

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 "use strict";
 
 function handleMenu() {
-  const navUl = document.getElementById("nav-ul");
   const navDailog = document.getElementById("nav-dailog");
   navDailog.classList.toggle("hidden");
 }
