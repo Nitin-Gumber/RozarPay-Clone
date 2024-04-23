262f5506787dac1bc9e286fc689f6148c19b29f5
Nitin Gumber <aroranitin486@gmail.com>
2024-04-23 11:56:35 +0530

    Add: separate js file complete: hero section

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+function handleMenu() {
+  const navUl = document.getElementById("nav-ul");
+  const navDailog = document.getElementById("nav-dailog");
+  navDailog.classList.toggle("hidden");
+}
