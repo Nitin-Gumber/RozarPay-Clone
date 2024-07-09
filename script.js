802d93c5dc22956cfe706a6d3d8059afbb6dca66
Nitin Gumber <aroranitin486@gmail.com>
2024-07-09 13:23:32 +0530

    add: javaScript module

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+const navDailog = document.getElementById("nav-dailog");
+const openMenu = document.getElementById("open-menu");
+const closeMenu = document.getElementById("close-menu");
+function handleMenu() {
+  navDailog.classList.toggle("top-[4rem]");
+  openMenu.classList.toggle("hidden");
+  closeMenu.classList.toggle("hidden");
+}
